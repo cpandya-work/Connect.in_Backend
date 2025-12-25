8ce46f610e3e4262666e86b8b016970d3d9bb18d
jeel2410 <vachhanijeel2001@gmail.com>
2025-12-25 23:30:08 +0530

    fix: updated bug

--- src/controllers/feed.controller.js
+++ src/controllers/feed.controller.js
@@ -51,7 +51,7 @@ const getFeedCtrl = asyncHandler(async (req, res) => {
   );
 
   if (profiles.length === 0) {
-    return res.status(204).json(); // No content
+    return res.status(200).json([]); // No content
   }
 
   success(res, { profiles, nextCursor }, 'Feed loaded');
