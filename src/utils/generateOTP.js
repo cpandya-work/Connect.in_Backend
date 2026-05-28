58594877a7190fa9795d87b7a29d458659449feb
jeel2410 <vachhanijeel2001@gmail.com>
2026-05-28 22:40:59 +0530

    fix: chnage in otp send

--- src/utils/generateOTP.js
+++ src/utils/generateOTP.js
@@ -1,6 +1,6 @@
 const generateOTP = () => {
-  // return Math.floor(100000 + Math.random() * 900000).toString(); // 6-digit
-  return '123456';
+   return Math.floor(100000 + Math.random() * 900000).toString(); // 6-digit
+ // return '123456';
 };
 
 module.exports = generateOTP;
